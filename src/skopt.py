38ff9f29672b476bb46907a485672c2b69191674
Akifumi Abe <akiabe0831@gmail.com>
2020-10-25 08:14:28 +0900

    update

--- src/skopt.py
+++ src/skopt.py
@@ -4,12 +4,10 @@ import numpy as np
 from sklearn import ensemble
 from sklearn import metrics
 from sklearn import model_selection
-from sklearn import decomposition
-from sklearn import preprocessing
-from sklearn import pipeline
 
 from functools import partial
 from skopt import space
+from skopt import gp_minimize
 
 def optimize(params, param_names, x, y):
     params = dict(zip(param_names, params))
@@ -31,19 +29,44 @@ def optimize(params, param_names, x, y):
 
     return -1.0 * np.mean(accuracies)
 
-
 if __name__ == "__main__":
     df = pd.read_csv("../input/train.csv")
     X = df.drop("price_range", axis=1).values
     y = df.price_range.values
 
     param_space = [
-
+        space.Integer(3, 15, name="max_depth"),
+        space.Integer(100, 600, name="n_estimators"),
+        space.Categorical(["gini", "entropy"], name="criterion"),
+        space.Real(0.01, 1, prior="uniform", name="max_features")
+    ]
+    param_names = [
+        "max_depth",
+        "n_estimators",
+        "criterion",
+        "max_features"
     ]
 
     optimization_function = partial(
         optimize,
-        param_names=,
+        param_names=param_names,
         x=X,
         y=y
+    )
+
+    result = gp_minimize(
+        optimization_function,
+        dimensions=param_space,
+        n_calls=15,
+        n_random_starts=10,
+        verbose=10
+    )
+
+    print(
+        dict(
+            zip(
+                param_names,
+                result.x
+            )
+        )
     )
