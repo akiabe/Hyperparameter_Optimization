522f6d3bd97c658882c5d8e990128807563c9449
Akifumi Abe <akiabe0831@gmail.com>
2020-10-25 06:34:29 +0900

    update

--- src/random_search.py
+++ src/random_search.py
@@ -4,21 +4,36 @@ import numpy as np
 from sklearn import ensemble
 from sklearn import metrics
 from sklearn import model_selection
+from sklearn import decomposition
+from sklearn import preprocessing
+from sklearn import pipeline
 
 if __name__ == "__main__":
     df = pd.read_csv("../input/train.csv")
     X = df.drop("price_range", axis=1).values
     y = df.price_range.values
 
-    clf = ensemble.RandomForestClassifier(n_jobs=-1)
+    scl = preprocessing.StandardScaler()
+    pca = decomposition.PCA()
+    rf = ensemble.RandomForestClassifier(n_jobs=-1)
+
+    classifier = pipeline.Pipeline(
+        [
+            ("scaling", scl),
+            ("pca", pca),
+            ("rf", rf)
+        ]
+    )
+
     param_grid = {
-        "n_estimators": np.arange(100, 1500, 100),
-        "max_depth": np.arange(1, 20),
-        "criterion": ["gini", "entropy"]
+        "pca__n_components": np.arange(5, 10),
+        "rf__n_estimators": np.arange(100, 1500, 100),
+        "rf__max_depth": np.arange(1, 20),
+        "rf__criterion": ["gini", "entropy"]
     }
 
     model = model_selection.RandomizedSearchCV(
-        estimator=clf,
+        estimator=classifier,
         param_distributions=param_grid,
         n_iter=10,
         scoring="accuracy",
